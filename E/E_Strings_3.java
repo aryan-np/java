21a448415ff24d06679991b2b15de6b480a5a089
aryan <nparyan7@gmail.com>
2024-06-03 22:40:42 +0545

    some que

--- E/E_Strings_3.java
+++ E/E_Strings_3.java
@@ -4,7 +4,7 @@ public class E_Strings_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string to convert in uppercase");
-        String str = sc.nextLine();
+         String str = sc.nextLine();
         System.out.println(str.toUpperCase());
         sc.close();
     }
